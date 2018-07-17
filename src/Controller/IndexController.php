9e1144ec5eef3a809c2d3f36f1e39ee0b74aee11
yun <yun.wu@infogreffe-siege.fr>
2018-07-17 11:17:01 +0200

    Add siren validation

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\IFG_SDPD\InfoToSaisie;
+use App\Entity\IFG_TEST2\InfoToSaisie;
 use App\Form\InfoToSaisieType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -20,6 +20,39 @@ class IndexController extends Controller
         $info = new InfoToSaisie();
         $form = $this->createForm(InfoToSaisieType::class, $info);
         $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $siren = $info->getSiren();
+
+            $em_TEST2 = $this->getDoctrine()->getManager('IFG_TEST2')->getConnection();
+            $sql = "
+                SELECT * FROM public.ta_suividem_ass p 
+                WHERE p.siren='".$siren."' and p.codetypeacte='BENh' ORDER BY dtdepot DESC ";
+            $info_saisie = $em_TEST2->prepare($sql);
+            $info_saisie->execute();
+            $infos_db = $info_saisie->fetchAll();
+
+            if (empty($infos_db)) {
+                return $this->render('error/siren.html.twig', [
+                    'siren' => $siren
+                ]);
+            } else {
+                $res = $infos_db[0];
+            }
+
+            $codegreffe = $res['codegreffe'];
+            $numgestion = $res['numerogestion'];
+            $numacte = $res['noacte'];
+            $numdepot = $res['numdepot'];
+
+            $codestatut = substr($numgestion, 4, 1);
+            $chrono = substr($numgestion, 5);
+            $millesime = substr($numgestion, 2, 2);
+
+            return $this->redirectToRoute('uploadpage', array('codegreffe'=>$codegreffe, 'codestatut'=>$codestatut, 'chrono'=>$chrono, 'millesime'=>$millesime, 'numacte'=>$numacte, 'numdepot'=>$numdepot));
+
+            // return new Response ("Code greffe: ".$codegreffe."<br>Numéro gestion: ".$numgestion."<br>Code statut: ".$codestatut."<br>Chrono: ".$chrono."<br>Millesime: ".$millesime."<br>Numéro d'acte: ".$numacte."<br>Numéro de dépôt: ".$numdepot."<br>".$url_GED);
+        }
 
         return $this->render('index/index.html.twig', [
             'form' => $form->createView(),
