e1c3136584407e3a65f22adc36c2e52b0a666c72
yun <yun.wu@infogreffe-siege.fr>
2018-07-24 17:13:30 +0200

    Add DBE-S-2 form additional

--- src/Controller/CompareController.php
+++ src/Controller/CompareController.php
@@ -123,22 +123,46 @@ class CompareController extends Controller
     }
 
     /**
-     * @Route("/addDBE_S_2/{filename}/{api_length}", name="addDBE_S_2page")
+     * @Route("/addDBE_S_2/{filename}/{beneficiaires}/{siren}", name="addDBE_S_2page")
      */
-    public function addDBE_S_2($filename, $api_length)
+    public function addDBE_S_2($filename, $beneficiaires, $siren)
     {
-        $api_orig = '{ "1":{"a":"xxxxxxxxxx", "b":"xxxxxxxxxx", "indirecte":"0"}, "2":{"a":"yyyyyyyyyyyyyyy", "b":"yyyyyyyyyyyyyyy", "indirecte":"1"}, "3":{"joint":"jointjointjoint", "b":"yyyyyyyyyyyyyyy"}}';
-        $apis = json_decode($api_orig, true);
+        /**
+         * API
+         */
+        $api_url_GET = "https://apidata.datainfogreffe.fr:8069/associes/rbe?siren=$siren";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $api_url_GET);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+        curl_setopt($ch, CURLOPT_USERPWD, 'infogreffe:3fn4rg2ff2');
+
+        $res_ch = curl_exec($ch);
+        curl_close($ch);
+        $apis = json_decode($res_ch, true);
+        
+        /**
+         * DB TEST2
+         */
+        $em_TEST2 = $this->getDoctrine()->getManager('IFG_TEST2')->getConnection();
+        $sql = "
+            SELECT * FROM public.ta_suividem_ass p 
+            WHERE p.siren='".$siren."' and p.codetypeacte='BENh' and p.dtsaisie is not null ORDER BY dtdepot DESC "; 
+        $info_saisie = $em_TEST2->prepare($sql);
+        $info_saisie->execute();
+        $infos_db = ($info_saisie->fetchAll())[0];
 
 
-        for ($i = (count($apis)-1); $i < $api_length; $i++) {
-            $api_merge = ["$i"=>["a"=>" ", "b"=>" ", "indirecte"=>"0"]];
-            $apis = array_merge($apis, $api_merge);
+        for ($i = (count($apis['beneficiaires'])-1); $i < $beneficiaires; $i++) {
+            $api_merge = ["$i"=>[" "=>" "]];
+            $apis['beneficiaires'] = array_merge($apis['beneficiaires'], $api_merge);
         }
 
         return $this->render('compare/compare.html.twig', [
             'filename' => $filename,
-            'apis' => $apis
+            'apis' => $apis,
+            'siren' => $siren,
+            'infos_db' => $infos_db
         ]); 
     }
 
